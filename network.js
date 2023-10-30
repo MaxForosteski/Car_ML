4cb449e3adad3c9e3bcc536fde888578089d1999
Max Forosteski <81583163+MaxForosteski@users.noreply.github.com>
2023-10-30 15:01:01 -0300

    Update network.js

--- network.js
+++ network.js
@@ -26,7 +26,7 @@ class NeuralNetwork{
                     amount
                 )
             }
-            for(let i = 0;i<level.weigths.length;i++){
+            for(let i = 0;i<level.weights.length;i++){
                 for(let j = 0;j < level.weights[i].length;j++){
                     level.weights[i][j] = lerp(
                         level.weights[i][j],
